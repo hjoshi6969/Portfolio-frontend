2457c884d0f8a2fd865d3217a800a7219fe99d23
Himanshu Joshi <himanshujoshi3304@gmail.com>
2024-01-01 19:44:01 -0600

    add a nav bar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React, { useEffect } from 'react'
-
+import { Button, Alert, Breadcrumb, cards, Card } from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+import Navigation from './Navigation'
 function App() {
-  useEffect(()=>{
-    fetch('http://localhost:8000/projects')
-    .then(res => res.json())
-    .then(data => console.log(data))
-  })
+  // useEffect(()=>{
+  //   fetch('http://localhost:8000/skills')
+  //   .then(res => res.json())
+  //   .then(data => console.log(data))
+  // })
   return (
-    <div>App</div>
+    <div>
+      <Navigation />
+    </div>
   )
 }
 
